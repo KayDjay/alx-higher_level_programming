266f3ad96e1f93d214799868dbd9094da9c3c2c3
root <Khadijatrasaq@gmail.com>
2023-08-30 22:00:52 -0700

    Task

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+
+    """Initialize the class object with size attributes """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Get size of the instances."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    """ Public area attribute """
+    def area(self):
+        return (self.__size * self.__size)
+
+    def __eq__(self, other):
+        """Define the == comparision to a Square."""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """Define the != comparison to a Square."""
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        """Define the < comparison to a Square."""
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """Define the <= comparison to a Square."""
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        """Define the > comparison to a Square."""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """Define the >= compmarison to a Square."""
+        return self.area() >= other.area()
