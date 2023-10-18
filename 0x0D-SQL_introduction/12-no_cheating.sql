c17511a62f2adb19a749c8d64c27cc22b46342e2
root <Khadijatrasaq@gmail.com>
2023-10-17 17:34:22 -0700

    Say my name

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- Changing the value of a column
+UPDATE second_table
+SET score = 10
+Where name = "BOB"
---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,2 @@
+-- Removing all records with a value <= 5
+DELETE FROM second_table WHERE score <= 5;
