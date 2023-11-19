c050cbfbff83a1196ff4a7f10710820f8daf34af
root <Khadijatrasaq@gmail.com>
2023-11-19 06:39:28 -0800

    Cities and Filtering

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -3,7 +3,7 @@
 """
 This module lists all states from the database
 The script will take 3 arguements: my sql username, mysql password
-and databas.
+and database with name starting with N.
 """
 
 from sys import argv
@@ -17,7 +17,10 @@ if __name__ == "__main__":
             passwd=argv[2],
             db=argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states")
+    cursor.execute("SELECT cities.id, cities.name, states.name\
+            FROM cities LEFT OUTER JOIN states\
+            ON states.id = cities.state_id\
+            ORDER BY cities.id ASC")
     states = (cursor.fetchall())
     for state in states:
         print(state)
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+""" The module lists all states from the database hbtn_0e_0_usa """
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    """    args = argv[1:]"""
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+    cursor = db.cursor()
+    query = "SELECT GROUP_CONCAT(name ORDER BY id ASC SEPARATOR ', ') \
+        FROM cities WHERE state_id = \
+        (SELECT id FROM states WHERE name = %s)"
+    cursor.execute(query, (argv[4],))
+    states = cursor.fetchall()
+    for state in states:
+        for i in state:
+            if i:
+                print(i)
+    cursor.close()
