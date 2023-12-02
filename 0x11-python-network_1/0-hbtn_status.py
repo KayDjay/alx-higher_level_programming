eac92cdfd4ea3892c3a8d60aaef3e3aef78d2004
root <Khadijatrasaq@gmail.com>
2023-12-01 18:13:59 -0800

    Response&READMe

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Python script that fetches URLs response"""
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print(f"Body response:\n"
+              f"\t- type: {type(html)}\n"
+              f"\t- content: {repr(html)}\n"
+              f"\t- utf8 content: {html.decode('utf-8')}")
